fc126c67559fb47aa3c36ffd8c2739837398935b
Liz Sugar <liz@lizsugar.me>
2022-05-13 13:28:13 -0700

    reduce height of cards a tad

--- dist/slider-button-card.js
+++ dist/slider-button-card.js
@@ -7732,9 +7732,9 @@ let SliderButtonCard = class SliderButtonCard extends LitElement {
         return css `
     ha-card {
       box-sizing: border-box;
-      height: 100%;
+      height: 90%;
       width: 100%;
-      min-height: 7rem;
+      min-height: 6rem;
       display: flex;
       flex-direction: column;
       justify-content: space-between;
@@ -7779,7 +7779,7 @@ let SliderButtonCard = class SliderButtonCard extends LitElement {
       padding: 0.8rem;
       box-sizing: border-box;
       height: 100%;
-      min-height: 7rem;
+      min-height: 6rem;
       width: 100%;
       display: block;
       overflow: hidden;           
@@ -7829,7 +7829,7 @@ let SliderButtonCard = class SliderButtonCard extends LitElement {
     
     .text {
       position: absolute;
-      bottom: 0;
+      bottom: -5px;
       left: 0;
       padding: 0.8rem;
       pointer-events: none;
--- src/slider-button-card.ts
+++ src/slider-button-card.ts
@@ -432,9 +432,9 @@ export class SliderButtonCard extends LitElement implements LovelaceCard {
     return css`
     ha-card {
       box-sizing: border-box;
-      height: 100%;
+      height: 90%;
       width: 100%;
-      min-height: 7rem;
+      min-height: 6rem;
       display: flex;
       flex-direction: column;
       justify-content: space-between;
@@ -479,7 +479,7 @@ export class SliderButtonCard extends LitElement implements LovelaceCard {
       padding: 0.8rem;
       box-sizing: border-box;
       height: 100%;
-      min-height: 7rem;
+      min-height: 6rem;
       width: 100%;
       display: block;
       overflow: hidden;           
@@ -529,7 +529,7 @@ export class SliderButtonCard extends LitElement implements LovelaceCard {
     
     .text {
       position: absolute;
-      bottom: 0;
+      bottom: -5px;
       left: 0;
       padding: 0.8rem;
       pointer-events: none;
